d8bcaa74f0d7457b32eb5900a3b79ea6558cb4db
Javier Valladares <javier.valladares@sansano.usm.cl>
2020-11-11 20:34:57 -0300

    actualizacion

--- Codigo/codigo_py.py
+++ Codigo/codigo_py.py
@@ -31,10 +31,12 @@ model.D = pyo.Param(model.J)
 model.Cont = pyo.Param(model.J)
 
 #Variables de decision
-model.x = pyo.Var(model.J, domain=pyo.Binary)
-model.p = pyo.Var(model.I, domain=pyo.NonNegativeReals)
-model.t_pro = pyo.Var(model.I, domain=pyo.PositiveIntegers)
-model.t_tra = pyo.Var(model.J, domain=pyo.PositiveIntegers)
+model.x = pyo.Var(model.J, within=pyo.Binary)
+model.p = pyo.Var(model.I, within=pyo.NonNegativeReals,bounds=(0,1700))
+model.t_pro = pyo.Var(model.I, within=pyo.NonNegativeReals, bounds=(1,12), initialize=1)
+model.t_tra = pyo.Var(model.J, within=pyo.NonNegativeReals, bounds=(4,8), initialize=4)
+
+
 
 
 #Funcion objetivo
@@ -48,29 +50,42 @@ def restriccion_precio_sup(m,i):
     return m.precio_etico >= m.p[i] 
 model.Precio_r_s = pyo.Constraint(model.I, rule=restriccion_precio_sup)
 
+
+def restriccion_horasproduccion(m,i):
+    return 12 >= m.t_pro[i]
+model.horasprod = pyo.Constraint(model.I,rule= restriccion_horasproduccion)
+
+
 def restriccion_precio_inf(m,i):
-    return m.p[i] >= m.C[i] - m.gan_min 
+    return m.p[i] >= m.C[i] + m.gan_min 
 model.Precio_r_i = pyo.Constraint(model.I, rule=restriccion_precio_inf)
+
 
 def restriccion_trabajadores(m):
     return  m.tra_permitidos >=  sum(m.x[j] for j in m.J)
 model.Max_trab = pyo.Constraint(rule = restriccion_trabajadores)
 
+
 def restriccion_trabajadores_inf(m):
     return    sum(m.x[j] for j in m.J) >= m.min_tra
 model.Min_trab = pyo.Constraint(rule = restriccion_trabajadores_inf)
+
 
 def restriccion_contaminacion(m):
     return  m.cont_max >= sum(m.x[j]*m.S[j]*m.Cont[j] for j in m.J) 
 model.Max_cont = pyo.Constraint(rule = restriccion_contaminacion)
 
+
 def restriccion_comuna(m,j):
         return  m.F[j] - m.fase >= m.x[j] 
 model.Comuna_r = pyo.Constraint(model.J, rule = restriccion_comuna)
 
+
 def restriccion_horastrabajo_i(m,j):
     return m.t_tra[j]>=4
 model.horas_i = pyo.Constraint(model.J,rule = restriccion_horastrabajo_i)
+
+
 def restriccion_horastrabajo_s(m,j):
     return 8 >= m.t_tra[j]
 model.horas_s = pyo.Constraint(model.J,rule = restriccion_horastrabajo_s)
@@ -80,6 +95,9 @@ def restriccion_produccion(m,i):
     return m.Q[i]*m.t_pro[i] >= m.prod_min  
 model.Produccion_r = pyo.Constraint(model.I,rule = restriccion_produccion)
 
+
 def restriccion_presupuesto(m):
     return m.presupuesto >= sum(m.Q[i]*m.C[i]*m.t_pro[i] for i in m.I) + sum(m.x[j]*m.S[j]*m.t_tra[j] for j in m.J) 
 model.Presupuesto_r = pyo.Constraint(rule = restriccion_presupuesto)
+
+
