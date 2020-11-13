89830bb60fae26872453b81bb46aedcc2ebe8122
Javier Valladares <javier.valladares@sansano.usm.cl>
2020-11-13 14:52:43 -0300

    actualizacion codigo

--- Codigo/codigo_py.py
+++ Codigo/codigo_py.py
@@ -101,6 +101,9 @@ def restriccion_produccion(m,i): #Restriccion de horas minimas de un trabajador
     return 30*m.Q[i]*m.t_pro[i] >= m.prod_min  
 model.Produccion_r = pyo.Constraint(model.I,rule = restriccion_produccion)
 
+def restriccion_hrsproduccion(m,i):
+    return 360 >= t_pro[i]
+model.hrsproduccion = pyo.COnstraint(model.I, rule = restriccion_hrsproduccion)
 
 def restriccion_presupuesto(m): #Restriccion de presupuesto de la empresa
     return m.presupuesto >= 30*sum(m.Q[i]*m.C[i]*m.t_pro[i] for i in m.I) + sum(m.x[j]*m.S[j]*m.t_tra[j] for j in m.J) 
