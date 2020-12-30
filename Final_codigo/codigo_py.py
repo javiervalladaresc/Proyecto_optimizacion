3f85e5642046c62a859b7cd5dd008c10f7b2221a
Javier Valladares <javier.valladares@sansano.usm.cl>
2020-12-30 19:18:36 -0300

    Modificacion datos

---
+++ Final_codigo/codigo_py.py
@@ -0,0 +1,112 @@
+from __future__ import division
+import pyomo.environ as pyo
+
+model = pyo.AbstractModel()
+
+#Parametros fijos de nuestro modelo
+model.gastos_fijos = pyo.Param(within = pyo.NonNegativeIntegers)
+model.tra_permitidos = pyo.Param(within = pyo.NonNegativeIntegers)
+model.cont_max = pyo.Param(within = pyo.NonNegativeIntegers)
+model.prod_min = pyo.Param(within = pyo.NonNegativeIntegers)
+model.precio_etico = pyo.Param(within = pyo.NonNegativeIntegers)
+model.gan_min = pyo.Param(within = pyo.NonNegativeIntegers)
+model.presupuesto = pyo.Param(within = pyo.NonNegativeIntegers)
+model.fase = pyo.Param(within = pyo.NonNegativeIntegers)
+model.min_tra = pyo.Param(within = pyo.NonNegativeIntegers)
+
+#Largo de los datos
+model.n = pyo.Param(within = pyo.NonNegativeIntegers) #Numero de trabajadores
+model.m = pyo.Param(within = pyo.NonNegativeIntegers) #Numero de alimentos
+
+#Indices de nuestros datos
+model.I = pyo.RangeSet(1, model.m)
+model.J = pyo.RangeSet(1, model.n)
+
+#Parametros de alimentos
+model.C = pyo.Param(model.I)
+model.Q = pyo.Param(model.I)
+
+
+#Parametros de trabajadores
+model.S = pyo.Param(model.J)
+model.F = pyo.Param(model.J)
+model.D = pyo.Param(model.J)
+model.Cont = pyo.Param(model.J)
+
+#Variables de decision
+model.x = pyo.Var(model.J, within=pyo.Binary)
+model.p = pyo.Var(model.I, within=pyo.NonNegativeReals,bounds=(0,1700))
+model.t_pro = pyo.Var(model.I, within=pyo.NonNegativeReals, bounds=(1,12), initialize=1)
+model.t_tra = pyo.Var(model.J, within=pyo.NonNegativeReals, bounds=(0,8), initialize=0)
+
+
+
+
+#Funcion objetivo
+def obj_expresion(m):
+    return -30*sum(m.Q[i]*m.p[i]*m.t_pro[i] for i in m.I) + 30*sum(m.Q[i]*m.C[i]*m.t_pro[i] for i in m.I) + sum(m.x[j]*m.S[j]*m.t_tra[j] for j in m.J) + m.gastos_fijos
+
+model.OBJ = pyo.Objective(rule = obj_expresion)
+
+#Restricciones
+def restriccion_precio_sup(m,i): #Restriccion para un precio etico 
+    return m.precio_etico >= m.p[i] 
+model.Precio_r_s = pyo.Constraint(model.I, rule=restriccion_precio_sup)
+
+
+def restriccion_horasproduccion(m,i): #Restriccion para las horas de produccion
+    return 12 >= m.t_pro[i]
+model.horasprod = pyo.Constraint(model.I,rule= restriccion_horasproduccion)
+
+
+def restriccion_precio_inf(m,i): #Restriccion para una ganancia minima
+    return m.p[i] >= m.C[i] + m.gan_min 
+model.Precio_r_i = pyo.Constraint(model.I, rule=restriccion_precio_inf)
+
+
+def restriccion_trabajadores(m): #Restriccion de numero de trabajadores por salud 
+    return  m.tra_permitidos >=  sum(m.x[j] for j in m.J)
+model.Max_trab = pyo.Constraint(rule = restriccion_trabajadores)
+
+
+def restriccion_trabajadores_inf(m): #Restriccion de la cantidad minima de trabajadores
+    return    sum(m.x[j] for j in m.J) >= m.min_tra
+model.Min_trab = pyo.Constraint(rule = restriccion_trabajadores_inf)
+
+
+def restriccion_contaminacion(m): #Restriccion para la contaminacion maxima permitida
+    return  m.cont_max >= sum(m.x[j]*m.S[j]*m.Cont[j] for j in m.J) 
+model.Max_cont = pyo.Constraint(rule = restriccion_contaminacion)
+
+
+def restriccion_comuna(m,j): #Restriccion por comuna en cuarentena
+    return  m.F[j] - m.fase >= m.x[j] 
+model.Comuna_r = pyo.Constraint(model.J, rule = restriccion_comuna)
+
+def activacion_horastrabajo(m,j): #Activacion variable tiempo
+    return 1000*m.x[j] >= m.t_tra[j]
+model.activar_var = pyo.Constraint(model.J,rule = activacion_horastrabajo)
+
+def restriccion_horastrabajo_i(m,j): #Restriccion de horas de trabajo
+    return  m.t_tra[j] - 80 + 1000*(1-m.x[j]) >= 0
+model.horas_i = pyo.Constraint(model.J,rule = restriccion_horastrabajo_i)
+
+
+def restriccion_horastrabajo_s(m,j): #Restriccion de horas de trabajo maximo para un trabajador
+    return 1000*(1-m.x[j]) >= m.t_tra[j] - 160
+model.horas_s = pyo.Constraint(model.J,rule = restriccion_horastrabajo_s)
+
+
+def restriccion_produccion(m,i): #Restriccion de horas minimas de un trabajador
+    return 30*m.Q[i]*m.t_pro[i] >= m.prod_min  
+model.Produccion_r = pyo.Constraint(model.I,rule = restriccion_produccion)
+
+def restriccion_hrsproduccion(m,i): #Restriccion de horas maximas de produccion
+    return 360 >= m.t_pro[i]
+model.hrsproduccion = pyo.Constraint(model.I, rule = restriccion_hrsproduccion)
+
+def restriccion_presupuesto(m): #Restriccion de presupuesto de la empresa
+    return m.presupuesto >= 30*sum(m.Q[i]*m.C[i]*m.t_pro[i] for i in m.I) + sum(m.x[j]*m.S[j]*m.t_tra[j] for j in m.J) 
+model.Presupuesto_r = pyo.Constraint(rule = restriccion_presupuesto)
+
+
---
+++ Final_codigo/nombres.py
@@ -0,0 +1,76 @@
+import random
+
+lista_nombres = ['raul','manuel','cristobal','diego','andres','claudio','javier',
+                        'roberto','pedro','juan','antonio','alonso','sebastian','carlos','mauricio',
+                        'pierre','alex','humberto','cristian','victor','felipe','jorge','aldo','roman',
+                        'ximena','jimena','maria','laura','javiera','paula','pamela','juana','antonia','paloma','macarena',
+                        'roberta','maria','valentina','alejandra','claudia','carla','lucia']
+
+lista_de_apellidos = ['garcia','rodriguez','martinez','hernandez','lopez','gonzalez','perez','sanchez',
+                        'ramirez','torres','flores','rivera','gomez','diaz','reyes','cruz','morales','ortiz','gutierrez']
+
+
+comunas = {'santiago' : 5, 'renca':10,'maipu':17,'puente_alto':27,'san_bernardo':20,'sano_joaquin':10,
+            'la_pintana':24,'el_bosque':16,'penalolen':12,'cerro_navia':10,'pudahuel':12,'macul':9,'cerrillos':10,
+            'la_florida':20,'recoleta':7,'la_granja':18}
+
+contaminacion = [0,0.53,0.16]
+
+sueldos = [3000,2500,2700]
+
+fase = [2,3,4]
+
+file = open('datos.csv','w')
+
+random.seed(42)
+
+file.write('nombre,apellido,comuna,fase,distancia,contaminacion,sueldoxhora \n' )
+
+contaminacion_codigo = []
+sueldo_codigo = []
+distancia_codigo = []
+fase_codigo = []
+
+
+for i in range(1300):
+    nombre = random.choice(lista_nombres)
+    apellido = random.choice(lista_de_apellidos)
+    comuna = random.choice(list(comunas.keys()))
+    distancia = str(comunas[comuna])
+    sueldo = str(random.choice(sueldos ))
+    fase = '2'
+    cont = str(random.choice(contaminacion))
+    contaminacion_codigo.append(cont)
+    sueldo_codigo.append(sueldo)
+    distancia_codigo.append(distancia)
+    fase_codigo.append(fase)
+
+    file.write(nombre + ',' + apellido + ',' + comuna + ',' + fase
+                + ',' + distancia + ',' + cont + ',' + sueldo
+                + '\n')
+
+file.close()
+
+file = open('datos_codigo.dat','w')
+
+param = ['F','S','D','Cont']
+
+for par in param:
+    file.write('param '+par+':= \n')
+    if par == 'F':
+        for k in range(1300):
+            file.write(str(k+1) + ' ' + str(fase_codigo[k]) + '\n')
+        file.write('; \n \n')
+    elif par == 'S':
+        for k in range(1300):
+            file.write(str(k+1) + ' ' + str(sueldo_codigo[k]) + '\n')
+        file.write('; \n \n')
+    elif par == 'D':
+        for k in range(1300):
+            file.write(str(k+1) + ' ' + str(distancia_codigo[k]) + '\n')
+        file.write('; \n \n')
+    elif par == 'Cont':
+        for k in range(1300):
+            file.write(str(k+1) + ' ' + str(contaminacion_codigo[k]) + '\n')
+        file.write('; \n \n')
+file.close()
